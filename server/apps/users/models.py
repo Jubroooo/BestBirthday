02f855aa3b15100ee0e6ec41f8735bcb85ce223f
jubro <ljh06050605@gmail.com>
2024-01-29 10:55:00 +0900

    [Minor] 유저 모델 생성

--- server/apps/users/models.py
+++ server/apps/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class user(AbstractUser) : 
+    name = models.CharField('이름', max_length=10)
+    nickname = models.CharField('닉네임', max_length=24)
+    birthday = models.DateField('생일')
+    toss_account = models.CharField('토스 송금링크', max_length=100, blank=True, null=True)
+    kakao_account = models.CharField('카카오 송금링크', max_length=100, blank=True, null=True)
+    
