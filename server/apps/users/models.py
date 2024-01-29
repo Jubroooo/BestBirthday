8fcbf87a811e3a7a0fe928eb5b736020f604fac1
LeeJinHyeok <kmer1024@hanyang.ac.kr>
2024-01-29 20:01:30 +0900

    [login]Django 로그인 구현

--- server/apps/users/models.py
+++ server/apps/users/models.py
@@ -7,6 +7,6 @@ class User(AbstractUser) :
     name = models.CharField('이름', max_length=10, null=True)
     username = models.CharField('아이디', unique=True, max_length=16, null=True)
     nickname = models.CharField('닉네임', max_length=24, null=True)
-    birthday = models.DateField('생일')
+    birthday = models.DateField('생일', null = True)
     toss_account = models.CharField('토스 송금링크', max_length=100, blank=True, null=True)
     kakao_account = models.CharField('카카오 송금링크', max_length=100, blank=True, null=True)
