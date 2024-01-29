57d247b44af0ffd4f9133588b19e84ee8473f2dd
jubro <ljh06050605@gmail.com>
2024-01-29 20:34:25 +0900

    [Minor] url, 템플릿 통일

--- server/apps/users/urls.py
+++ server/apps/users/urls.py
@@ -4,8 +4,7 @@ from .views import *
 app_name = 'users'
 
 urlpatterns = [
-    path('', main, name="main"),
     path('signup/', signup, name="signup"),
     path('login/', login, name="login"),
-    path('logout', logout, name="logout"),
+    path('logout/', logout, name="logout"),
 ]
