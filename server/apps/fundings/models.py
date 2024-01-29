0f984d122ea556fdc711e73bda7517e6874e01b6
LeeJinHyeok <kmer1024@hanyang.ac.kr>
2024-01-29 22:43:14 +0900

    [login]카카오 소셜 로그인 재구현

---
+++ server/apps/fundings/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from apps.users.models import User
+
+class Funding(models.Model) :
+    title = models.CharField('제목', max_length=24)
+    content = models.TextField('내용', max_length=1000)
+    goal_price = models.IntegerField('목표금액', default=0)
+    funding_price = models.IntegerField('받은금액', default=0, null=True)
+    photo = models.ImageField('이미지', blank=True, upload_to='posts/%Y%m%d')
+    present_link = models.CharField('상품 링크', max_length=100)
+    
+    #작성자
+    user=models.ForeignKey(User, on_delete=models.CASCADE, verbose_name = '작성자', related_name='funding_user')
+    #펀딩 개설일
+    created_date = models.DateTimeField('작성일', auto_created=True, auto_now_add=True) #auto_now_add > 데이터베이스에 추가될때 // auto_create 생성해라
+    
+    def __str__(self):
+        return f'{self.title}'
+    
+class Funding_Msg(models.Model) :
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name = '작성자', related_name='funding_msg_user')
+    post = models.ForeignKey(Funding, on_delete=models.CASCADE, verbose_name = '펀딩글', related_name="funding_post")
+    content = models.TextField() 
---
+++ server/apps/users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser) : 
+    first_name=None
+    last_name=None
+    name = models.CharField('이름', max_length=10, null=True)
+    username = models.CharField('아이디', unique=True, max_length=16, null=True)
+    nickname = models.CharField('닉네임', max_length=24, null=True)
+    birthday = models.DateField('생일', null = True)
+    toss_account = models.CharField('토스 송금링크', max_length=100, blank=True, null=True)
+    kakao_account = models.CharField('카카오 송금링크', max_length=100, blank=True, null=True)
