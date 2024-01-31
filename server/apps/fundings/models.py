db153f62f675ec6eb2387cc1bbc20d5f039f5f98
jubro <ljh06050605@gmail.com>
2024-01-31 10:16:41 +0900

    [Minor] media 추가, 연결해야됨

--- server/apps/fundings/models.py
+++ server/apps/fundings/models.py
@@ -5,8 +5,8 @@ class Funding(models.Model) :
     title = models.CharField('제목', max_length=24)
     content = models.TextField('내용', max_length=1000)
     goal_price = models.IntegerField('목표금액', default=0)
-    funding_price = models.IntegerField('받은금액', default=0, null=True)
-    photo = models.ImageField('이미지', blank=True, upload_to='posts/%Y%m%d')
+    total_price = models.IntegerField('받은금액', default=0, null=True)
+    photo = models.ImageField('이미지', blank=True, upload_to='fundings/%Y%m%d')
     present_link = models.CharField('상품 링크', max_length=100)
     
     #작성자
@@ -20,4 +20,6 @@ class Funding(models.Model) :
 class Funding_Msg(models.Model) :
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name = '작성자', related_name='funding_msg_user')
     post = models.ForeignKey(Funding, on_delete=models.CASCADE, verbose_name = '펀딩글', related_name="funding_post")
+    comment_name = models.CharField('From', max_length=12)
+    funding_price = models.IntegerField('선물금액', default=0, null=True)
     content = models.TextField() 
--- server/apps/fundings/views.py
+++ server/apps/fundings/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import FundingForm
+from .forms import FundingForm, MessageForm
 from .models import Funding, Funding_Msg
 
 
@@ -12,17 +12,19 @@ def create(request) :
     if request.method == 'GET':
         form = FundingForm()
         ctx = {'form':form}
-        return render(request, 'fundings/funding_create.html', ctx)
+        return render(request, 'fundings/fundings_create.html', ctx)
     #post일때
     form = FundingForm(request.POST, request.FILES)
     if form.is_valid():
-        form.save()
-    return redirect('fundings:detail')
+        new_funding = form.save(user=request.user)
+        pk_of_new_funding = new_funding.pk
+        return redirect('fundings:detail', pk=pk_of_new_funding)
 
 def detail(request, pk) :
     funding = Funding.objects.get(id=pk)
-    ctx = {'funding':funding}
-    return render(request, 'fundings/funding_detail.html', ctx)
+    progress = int(funding.total_price / funding.goal_price * 100)
+    ctx = {'funding':funding, 'progress':progress}    
+    return render(request, 'fundings/fundings_detail.html', ctx)
 
 def delete(request, pk) :
     if request.method == "POST":
@@ -37,9 +39,19 @@ def update(request, pk) :
     if request.method=='GET':
         form = FundingForm(instance=post)
         ctx = {'form':form, 'pk':pk}
-        return render(request, 'fundings/funding_update.html', ctx)
+        return render(request, 'fundings/fundings_update.html', ctx)
     
     form=FundingForm(request.POST, request.FILES, instance=post)
     if form.is_valid():
         form.save()
     return redirect('fundings:detail', pk)
+
+def message(request) :
+    if request.method == 'POST':
+        form = MessageForm(request.POST, request.FILES, request=request, current_post=current_post)
+        if form.is_valid():
+            # Save the form
+            form.save()
+            # Your additional logic here
+    else:
+        form = MessageForm(request=request, current_post=current_post)
