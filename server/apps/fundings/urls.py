22483deec6f15512cb62346ba0e017a6e20c254f
serenityjung <wjd7542@cau.ac.kr>
2024-02-03 00:35:13 +0900

    [Fix] merge conflict 해결

--- server/apps/fundings/urls.py
+++ server/apps/fundings/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
     # path('detail/<int:pk>/', detail, name="detail"), 
     path('delete/<int:pk>/', delete, name="delete"), 
     path('update/<int:pk>/', update, name="update"),
+    # 예진 추가뷰
+    #path('all_birthday_list/',all_list),
+    #뷰 확인용
+    # path('detail/', detail, name="detail"),
     path('detail/<int:pk>/message_create/', create_message, name="create_message"),
     path('list/today_funding/', today_funding, name="today_funding"),
     path('list/msg_funding/', msg_funding, name="msg_funding"),
@@ -16,7 +20,6 @@ urlpatterns = [
     path('view_messages/<int:pk>/', view_messages, name="view_messages"),
     path('view_all_messages/<int:pk>/', view_all_messages, name="view_all_messages"),
     path('funding_msg_detail/<int:pk>/', funding_msg_detail, name="funding_msg_detail"),
-    path('message/create', message, name="message"),
     # 예진 추가뷰
     path('all_birthday_list/',all_list),
     #뷰 확인용
