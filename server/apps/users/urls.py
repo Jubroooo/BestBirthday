5a493ea12f2b8809a6f9a462a178b9131c6789c8
LeeJinHyeok <kmer1024@hanyang.ac.kr>
2024-02-05 15:01:36 +0900

    풀 전 커미

--- server/apps/users/urls.py
+++ server/apps/users/urls.py
@@ -4,8 +4,6 @@ from .views import *
 app_name = 'users'
 
 urlpatterns = [
-    path('signup/', signup, name="signup"),
-    path('login/', login, name="login"),
     path('logout/', logout, name="logout"),
     path('add_info/', add_info, name="add_info"),
     path('redirect_view/', redirect_view, name="redirect_view"),
