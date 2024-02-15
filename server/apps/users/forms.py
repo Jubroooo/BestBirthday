cecfe4f992a648b3003d936d324240d356d2eb6f
jubro <ljh06050605@gmail.com>
2024-02-15 14:50:58 +0900

    [feature] 생일 년/월/일 입력 기능

--- server/apps/users/forms.py
+++ server/apps/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
+from datetime import datetime, date
 
 class SignupForm(UserCreationForm):
     class Meta:
@@ -8,11 +9,39 @@ class SignupForm(UserCreationForm):
         fields = ['username', 'password1', 'password2', 'nickname', 'birthday']
         
 class UserProfileUpdateForm(forms.ModelForm):
+    birth_year = forms.IntegerField(label='출생 연도', required=True)
+    birth_month = forms.IntegerField(label='출생 월', required=True)
+    birth_day = forms.IntegerField(label='출생 일', required=True)
+
     class Meta:
         model = User
-        fields = ['nickname', 'birthday', 'profile']
-    nickname = forms.CharField(label='닉네임', max_length=24, required=True)
-    birthday = forms.DateField(label='생일', required=True)
+        fields = ['nickname', 'profile']
+
+    def clean_birthday(self):
+        birth_year = self.cleaned_data.get('birth_year')
+        birth_month = self.cleaned_data.get('birth_month')
+        birth_day = self.cleaned_data.get('birth_day')
+        
+        try:
+            birthday = date(birth_year, birth_month, birth_day)
+        except ValueError:
+            raise forms.ValidationError("올바른 날짜를 선택해주세요.")
+
+        return birthday
+
+    def save(self, commit=True):
+        user = super(UserProfileUpdateForm, self).save(commit=False)
+
+        #생일 합치기
+        birth_year = self.cleaned_data.get('birth_year')
+        birth_month = self.cleaned_data.get('birth_month')
+        birth_day = self.cleaned_data.get('birth_day')
+        user.birthday = date(birth_year, birth_month, birth_day)
+
+        if commit:
+            user.save()
+
+        return user
 
 class UserProfilesettingForm(forms.ModelForm):
     class Meta:
